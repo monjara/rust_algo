0dd8e01604775e66a12c700281b3d79b4d0df8c2
monjara <yaginumee@gmail.com>
2022-04-04 22:43:42 +0900

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use proconio::input;
-use itertools::Itertools;
+use itertools::{iproduct, Itertools};
 
 fn add(a: f32, b:f32)  -> (f32, String, bool) { (a + b, String::from("+"), false) }
 fn sub(a: f32, b:f32)  -> (f32, String, bool) { (a - b, String::from("-"), false) }
@@ -34,53 +34,48 @@ fn main() {
         let mut tmp = v.iter().filter(|&el| el != arr[0] && el != arr[1]).collect_vec();
         arr.append(&mut tmp);
 
-        for fn_1 in calc_funcs.iter() {
-            for fn_2 in calc_funcs.iter() {
-                for fn_3 in calc_funcs.iter() {
-                    let (ans_1, str_1, reverse_1) = fn_1(*arr[0], *arr[1]);
-                    let (ans_2, str_2, reverse_2) = fn_2(ans_1, *arr[2]);
-                    let (ans_3, str_3, reverse_3) = fn_3(ans_2, *arr[3]);
-                    if ans_3 == 10.0 {
-
-                        let (sa, sb, sc) = if reverse_3 {
-                            if reverse_2 {
-                                (str_3, str_2, str_1)
-                            } else {
-                                (str_3, str_1, str_2)
-                            }
-                        } else {
-                            if reverse_2 {
-                                (str_2, str_1, str_3)
-                            } else {
-                                (str_1, str_2, str_3)
-                            }
-                        };
-                        
-                        if !reverse_1 && !reverse_2 && !reverse_3 {
-                            output_1(*arr[0], *arr[1], *arr[2], *arr[3], sa, sb, sc);
-                        }
-                        else if reverse_1 && !reverse_2 && !reverse_3 {
-                            output_1(*arr[1], *arr[0], *arr[2], *arr[3], sa, sb, sc);
-                        }
-                        else if reverse_1 && reverse_2 && !reverse_3 {
-                            output_2(*arr[2], *arr[1], *arr[0], *arr[3], sa, sb, sc);
-                        }
-                        else if !reverse_1 && reverse_2 && !reverse_3 {
-                            output_2(*arr[2], *arr[0], *arr[1], *arr[3], sa, sb, sc);
-                        }
-                        else if reverse_1 && !reverse_2 && reverse_3 {
-                            output_3(*arr[3], *arr[1], *arr[0], *arr[2], sa, sb, sc);
-                        }
-                        else if !reverse_1 && !reverse_2 && reverse_3 {
-                            output_3(*arr[3], *arr[0],*arr[1],  *arr[2], sa, sb, sc);
-                        }
-                        else if !reverse_1 && reverse_2 && reverse_3 {
-                            output_4(*arr[3], *arr[2], *arr[0], *arr[1], sa, sb, sc);
-                        }
-                        else if reverse_1 && reverse_2 && reverse_3 {
-                            output_4(*arr[3], *arr[2], *arr[1], *arr[0], sa, sb, sc);
-                        }
+        for (fn_1, fn_2, fn_3) in iproduct!(calc_funcs.iter(), calc_funcs.iter(), calc_funcs.iter()) {
+            let (ans_1, str_1, reverse_1) = fn_1(*arr[0], *arr[1]);
+            let (ans_2, str_2, reverse_2) = fn_2(ans_1, *arr[2]);
+            let (ans_3, str_3, reverse_3) = fn_3(ans_2, *arr[3]);
+            if ans_3 == 10.0 {
+                let (sa, sb, sc) = if reverse_3 {
+                    if reverse_2 {
+                        (str_3, str_2, str_1)
+                    } else {
+                        (str_3, str_1, str_2)
                     }
+                } else {
+                    if reverse_2 {
+                        (str_2, str_1, str_3)
+                    } else {
+                        (str_1, str_2, str_3)
+                    }
+                };
+                
+                if !reverse_1 && !reverse_2 && !reverse_3 {
+                    output_1(*arr[0], *arr[1], *arr[2], *arr[3], sa, sb, sc);
+                }
+                else if reverse_1 && !reverse_2 && !reverse_3 {
+                    output_1(*arr[1], *arr[0], *arr[2], *arr[3], sa, sb, sc);
+                }
+                else if reverse_1 && reverse_2 && !reverse_3 {
+                    output_2(*arr[2], *arr[1], *arr[0], *arr[3], sa, sb, sc);
+                }
+                else if !reverse_1 && reverse_2 && !reverse_3 {
+                    output_2(*arr[2], *arr[0], *arr[1], *arr[3], sa, sb, sc);
+                }
+                else if reverse_1 && !reverse_2 && reverse_3 {
+                    output_3(*arr[3], *arr[1], *arr[0], *arr[2], sa, sb, sc);
+                }
+                else if !reverse_1 && !reverse_2 && reverse_3 {
+                    output_3(*arr[3], *arr[0],*arr[1],  *arr[2], sa, sb, sc);
+                }
+                else if !reverse_1 && reverse_2 && reverse_3 {
+                    output_4(*arr[3], *arr[2], *arr[0], *arr[1], sa, sb, sc);
+                }
+                else if reverse_1 && reverse_2 && reverse_3 {
+                    output_4(*arr[3], *arr[2], *arr[1], *arr[0], sa, sb, sc);
                 }
             }
         }
@@ -97,34 +92,30 @@ fn main() {
     ];
 
     for arr in arrs.iter() {
-        for fn_1 in calc_funcs.iter() {
-            for fn_2 in calc_funcs.iter() {
-                for fn_3 in calc_funcs.iter() {
-                    let (ans_1, str_1, reverse_1) = fn_1(arr[0], arr[1]);
-                    let (ans_2, str_2, reverse_2) = fn_2(arr[2], arr[3]);
-                    let (ans_3, str_3, reverse_3) = fn_3(ans_1, ans_2);
-                    if ans_3 == 10.0 {
-                        let mut ans_arr = arr.to_vec();
+        for (fn_1, fn_2, fn_3) in iproduct!(calc_funcs.iter(), calc_funcs.iter(), calc_funcs.iter()) {
+            let (ans_1, str_1, reverse_1) = fn_1(arr[0], arr[1]);
+            let (ans_2, str_2, reverse_2) = fn_2(arr[2], arr[3]);
+            let (ans_3, str_3, reverse_3) = fn_3(ans_1, ans_2);
+            if ans_3 == 10.0 {
+                let mut ans_arr = arr.to_vec();
 
-                        if reverse_1 {
-                            ans_arr.swap(0, 1);
-                        };
-                        if reverse_2 {
-                            ans_arr.swap(2, 3);
-                        };
-                        if reverse_3 { 
-                            ans_arr.swap(0, 2);
-                            ans_arr.swap(1, 3);
-                        };
+                if reverse_1 {
+                    ans_arr.swap(0, 1);
+                };
+                if reverse_2 {
+                    ans_arr.swap(2, 3);
+                };
+                if reverse_3 { 
+                    ans_arr.swap(0, 2);
+                    ans_arr.swap(1, 3);
+                };
 
-                        let (sa, sb, sc) = if reverse_3 {
-                            (str_2, str_3, str_1)
-                        } else {
-                            (str_1, str_3, str_2)
-                        };
-                        output_5(ans_arr[0], ans_arr[1], ans_arr[2], ans_arr[3], sa, sb, sc);
-                    }
-                }
+                let (sa, sb, sc) = if reverse_3 {
+                    (str_2, str_3, str_1)
+                } else {
+                    (str_1, str_3, str_2)
+                };
+                output_5(ans_arr[0], ans_arr[1], ans_arr[2], ans_arr[3], sa, sb, sc);
             }
         }
     }
