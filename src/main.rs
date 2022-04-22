2b9ca3a5be2dd6ecb1c728a337ffa6a816047cb5
monjara <yaginumee@gmail.com>
2022-04-22 19:53:31 +0900

    243-B

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,25 @@ use proconio::input;
 
 fn main() {
     input! {
-        v: usize,
-        a: usize,
-        b: usize,
-        c: usize,
+        n: i64,
+        a: [u64; n],
+        b: [u64; n],
     };
-    let rest = v % (a + b + c);
-    let ans = if rest < a {
-        'F'
-    } else if rest < a + b {
-        'M'
-    } else {
-        'T'
-    };
-    println!("{}", ans);
+    let mut a_ans = 0;
+    let mut b_ans = 0;
+    for i in 0..n {
+        for j in 0..n {
+            if i == j {
+                if a[i as usize] == b[j as usize] { a_ans += 1; }
+            } else {
+                if a[i as usize] == b[j as usize] { 
+                    b_ans += 1; 
+                    break;
+                }
+            }
+
+        }
+    }
+    println!("{}", a_ans);
+    println!("{}", b_ans);
 }
