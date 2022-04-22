af8f7fffcfd9aa687044c61934604ede4620a879
monjara <yaginumee@gmail.com>
2022-04-22 19:40:53 +0900

    244-B

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,27 @@ use proconio::input;
 
 fn main() {
     input! {
-        _: u32,
-        s: String,
+        _n: u64,
+        t: String,
     };
-    let ans = s.chars().last().unwrap();
-    println!("{}", ans);
+    let mut x = 0;
+    let mut y = 0;
+    let mut d = 0;
+    for c in t.chars() {
+        if c == 'R' {
+            d += 1;
+            d %= 4;
+        } else {
+            if d == 0 {
+                x += 1;
+            } else if d == 1 {
+                y -= 1;
+            } else if d == 2 {
+                x -= 1;
+            } else if d == 3 {
+                y += 1;
+            }
+        }
+    }
+    println!("{} {}", x, y);
 }
