720571c726a6b04b3ca6bc0a253385bc4a5cf339
monjara <yaginumee@gmail.com>
2022-04-22 17:43:32 +0900

    248-B

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use proconio::input;
 
 fn main() {
-    input! {s: String}
-    for i in 0..10 {
-        if !s.contains(&i.to_string()) {
-            println!("{}", i);
-            return;
-        }
+    input! {
+        mut a: f64,
+        b: f64,
+        k: f64,
     }
+    let mut ans = 0;
+    while b > a {
+        a *= k;
+        ans += 1;
+    }
+    println!("{}", ans);
 }
