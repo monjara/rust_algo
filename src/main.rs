96d80f8138a79aec2da767f1de13b9b15fbc3d94
monjara <yaginumee@gmail.com>
2022-04-22 19:09:58 +0900

    244-A

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,9 @@ use proconio::input;
 
 fn main() {
     input! {
-        mut v: [i32; 10],
-    }
-    v.sort_by(|a, b| b.cmp(a));
-    for i in 0..3 {
-        println!("{}", v[i]);
-    }
+        _: u32,
+        s: String,
+    };
+    let ans = s.chars().last().unwrap();
+    println!("{}", ans);
 }
