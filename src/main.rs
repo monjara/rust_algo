700c74b6396fb490e2639731c7b36046b842c9f5
monjara <yaginumee@gmail.com>
2022-04-23 10:38:19 +0900

    241-A

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,8 @@ use proconio::input;
 
 fn main() {
     input! {
-        a: f64,
-        b: f64,
-        c: f64,
-        x: f64,
+        a: [usize; 10],
     };
-    let ans = if x <= a {
-        1.00
-    } else if x <= b {
-        c / (b - a)
-    } else {
-        0.00
-    };
-    println!("{:.13}", ans);
+    let ans = a[a[a[0]]];
+    println!("{}", ans);
 }
