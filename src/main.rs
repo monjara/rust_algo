584fb5371df462359f1161a52271671b1e12faea
monjara <yaginumee@gmail.com>
2022-04-23 14:41:56 +0900

    208-B

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,31 @@
 use proconio::input;
 
+fn factorial(n: usize) -> usize {
+    if n == 1 {
+        n
+    } else {
+        n * factorial(n - 1)
+    }
+}
+
 fn main() {
     input! {
-        a: usize,
-        b: usize,
+        mut p: usize,
     };
-    let ans = if a <= b && b <= a * 6 { "Yes" } else { "No" };
+
+    let mut ans = 0;
+    for i in (1..=10).rev() {
+        while p >= factorial(i) {
+            ans += 1;
+            p -= factorial(i);
+        }
+    }
+
     println!("{}", ans);
+}
+
+#[test]
+fn test_factorial() {
+    assert_eq!(factorial(1), 1);
+    assert_eq!(factorial(10), 3628800);
 }
