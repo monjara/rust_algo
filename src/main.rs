0d6aaf29a531bf4f4793004dfee83fa147a00b0a
monjara <yaginumee@gmail.com>
2022-04-23 23:59:18 +0900

    binary_search https://qiita.com/drken/items/97e37dd6143e33a64c8c#1-%E6%99%AE%E9%80%9A%E3%81%AE%E4%BA%8C%E5%88%86%E6%8E%A2%E7%B4%A2%E3%81%8B%E3%82%89-stdlower_bound-%E3%81%B8

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
-use proconio::input;
+// use proconio::input;
+
+fn binary_search(key: usize, v: &Vec<usize>) -> isize {
+    let mut left = 0;
+    let mut right = v.len() - 1;
+    while right >= left {
+        let mid = left + (right - left) / 2;
+        if v[mid] == key {
+            return mid as isize;
+        } else if v[mid] > key {
+            right = mid - 1;
+        } else if v[mid] < key {
+            left = mid + 1;
+        }
+
+    }
+    return -1;
+}
 
 fn main() {
-    input! {
-        n: usize,
-        a: usize,
-        x: usize,
-        y: usize,
-    };
-    let ans = if n > a {
-        a * x + (n - a) * y
-    } else {
-        n * x
-    };
-    println!("{}", ans);
+    let v = vec![1, 14, 32, 51, 51, 51, 243, 419, 750, 910];
+    let index = binary_search(51, &v);
+    println!("{}", index);
 }
