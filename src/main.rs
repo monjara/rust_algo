280ec556a9a56a2e16cdf2f5112d20c807ca63dc
monjara <yaginumee@gmail.com>
2022-04-24 00:37:16 +0900

    https://atcoder.jp/contests/typical-algorithm/tasks/typical_algorithm_a

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,29 @@
-// use proconio::input;
-
-fn is_ok(right: usize, key: usize, v: &Vec<usize>) -> bool {
-    v[right] >= key
-}
+use proconio::input;
 
 fn binary_search(key: usize, v: &Vec<usize>) -> isize {
-    let mut left = -1;
-    let mut right = v.len() as isize;
+    let mut left = 0;
+    let mut right = v.len() - 1;
     while right - left > 1 {
         let mid = left + (right - left) / 2;
-        if is_ok(mid as usize, key, v) {
+        if v[mid] > key {
             right = mid;
         } else {
             left = mid;
         }
     }
-    right
+    right as isize
 }
 
 fn main() {
-    let v = vec![1, 14, 32, 51, 51, 51, 243, 419, 750, 910];
-    let index = binary_search(51, &v);
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; n],
+    }
+    let index = if k > a[n-1] {
+        -1
+    } else {
+        binary_search(k, &a)
+    };
     println!("{}", index);
 }
