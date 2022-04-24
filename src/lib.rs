be0fe3c7dcb9a6be322ab511bc5b707e0237b030
monjara <yaginumee@gmail.com>
2022-04-24 17:27:56 +0900

    bit 部分和問題

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 fn binary_search(key: usize, v: &Vec<usize>) -> isize {
     let mut left = -1;
-    let mut right = (v.len()-1) as isize;
+    let mut right = (v.len() - 1) as isize;
     while right - left > 1 {
         let mid = left + (right - left) / 2;
         if v[mid as usize] >= key {
@@ -19,4 +19,29 @@ fn test_binary_search() {
     assert_eq!(binary_search(51, &v), 3);
     assert_eq!(binary_search(910, &v), 9);
     assert_eq!(binary_search(1000, &v), 9);
+}
+
+fn bit_partial_sum(sum: usize, v: &Vec<usize>) -> bool {
+    let len = v.len();
+    let mut exist = false;
+    for bit in 0..(1 << len) {
+        let bit_list = (0..len)
+            .filter(|x| (bit & (1 << x)) != 0)
+            .map(|x| v[x as usize]);
+        let bit_sum: usize = bit_list.sum();
+        if sum == bit_sum {
+            exist = true;
+        };
+    }
+    if exist {
+        true
+    } else {
+        false
+    }
+}
+
+#[test]
+fn test_bit_partial_sum() {
+    assert_eq!(bit_partial_sum(10, &vec![1, 3, 6]), true);
+    assert_eq!(bit_partial_sum(10, &vec![2, 3, 6]), false);
 }
