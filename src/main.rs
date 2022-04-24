df6cf0b84af825ba26df670cbcaa7c29c8c674d9
monjara <yaginumee@gmail.com>
2022-04-24 17:17:23 +0900

    部分和

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,22 @@
 use proconio::input;
 
-fn solve(m: isize, k: &isize, l: &isize, v: &Vec<isize>) -> bool {
-    let mut cnt = 0;
-    let mut pre = 0;
-
-    for i in v {
-        if i - pre >= m && l - i >= m {
-            cnt += 1;
-            pre = *i;
-        };
-    }
-    if &cnt >= k {
-        true
-    } else {
-        false
-    }
-}
-
 fn main() {
     input! {
         n: isize,
-        l: isize,
-        k: isize,
+        w: isize,
         a: [isize; n],
     }
-
-    let mut left = -1;
-    let mut right = l + 1;
-
-    while right - left > 1 {
-        let mid = left + (right - left) / 2;
-        if solve(mid, &k, &l, &a) {
-            left = mid;
-        } else {
-            right = mid;
-        }
+    let mut exist = false;
+    for bit in 0..(1 << n) {
+        let sub_list = (0..n)
+            .filter(|x| (bit & (1 << x)) != 0)
+            .map(|x| a[x as usize]);
+        println!("");
+        let sum: isize = sub_list.sum();
+        if w == sum {
+            exist = true;
+        };
     }
-    println!("{}", left);
+    let ans = if exist { "Yes" } else { "No" };
+    println!("{}", ans);
 }
