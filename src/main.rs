6c5b92a219fdb47fa8feb81c9dd7b479e0f8f859
monjara <yaginumee@gmail.com>
2022-04-24 18:04:51 +0900

    https://atcoder.jp/contests/typical90/tasks/typical90_b

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,40 @@
 use proconio::input;
 
-fn main() {
-    input! {
-        n: isize,
-        w: isize,
-        a: [isize; n],
-    }
-    let mut exist = false;
-    for bit in 0..(1 << n) {
-        let sub_list = (0..n)
-            .filter(|x| (bit & (1 << x)) != 0)
-            .map(|x| a[x as usize]);
-        println!("");
-        let sum: isize = sub_list.sum();
-        if w == sum {
-            exist = true;
+fn check(s: &str) -> bool {
+    let mut check = 0;
+    for c in s.chars() {
+        if c == '(' {
+            check += 1
+        } else if c == ')' {
+            check -= 1
+        }
+        if check < 0 {
+            return false;
         };
     }
-    let ans = if exist { "Yes" } else { "No" };
-    println!("{}", ans);
+    if check == 0 {
+        true
+    } else {
+        false
+    }
+}
+
+fn main() {
+    input! {
+        n: usize,
+    }
+
+    for bit in 0..(1 << n) {
+        let mut str = "".to_string();
+        for i in (0..=n - 1).rev() {
+            if (bit & (1 << i)) == 0 {
+                str.push_str("(");
+            } else {
+                str.push_str(")");
+            }
+        }
+        if check(&str) {
+            println!("{}", str);
+        };
+    }
 }
