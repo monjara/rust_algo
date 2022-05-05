e6154e6a77466604f9ec113db6975ed0938c9ede
monjara <yaginumee@gmail.com>
2022-05-05 14:10:34 +0900

    218-A

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,14 @@ use proconio::input;
 
 fn main() {
     input! {
-        s_1: String,
-        s_2: String,
-        s_3: String,
-        t: String,
+        mut n: usize,
+        s: String,
     }
-    for i in t.chars() {
-        match i {
-            '1' => print!("{}", &s_1),
-            '2' => print!("{}", &s_2),
-            '3' => print!("{}", &s_3),
-            _ => {},
-        }
-    }
-    println!();
+    n -= 1;
+    let ans = if s.chars().nth(n).unwrap() == 'o' {
+        "Yes"
+    } else {
+        "No"
+    };
+    println!("{}", ans)
 }
