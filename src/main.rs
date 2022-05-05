61adb0e0ddb146762182a8109575400ac57152dd
monjara <yaginumee@gmail.com>
2022-05-05 15:00:22 +0900

    214-B

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,18 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
+        s: usize,
+        t: usize,
     }
-    let ans = if n <= 125 {
-        4
-    } else if n <= 211 {
-        6
-    } else {
-        8
-    };
+    let mut ans = 0;
+    for a in 0..=s {
+        for b in 0..=s {
+            for c in 0..=s {
+                if a + b + c <= s && a * b * c <= t {
+                    ans += 1;
+                }
+            }
+        }
+    }
     println!("{}", ans);
 }
