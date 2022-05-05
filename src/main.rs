5552e5203a466b999e325688a015574fbc06cc5b
monjara <yaginumee@gmail.com>
2022-05-05 14:18:50 +0900

    218-B

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,9 @@ use proconio::input;
 
 fn main() {
     input! {
-        mut n: usize,
-        s: String,
+        a: [u8; 26]
     }
-    n -= 1;
-    let ans = if s.chars().nth(n).unwrap() == 'o' {
-        "Yes"
-    } else {
-        "No"
-    };
-    println!("{}", ans)
+    for num in a {
+        print!("{}", ('a' as u8 + num - 1) as char);
+    }
 }
