4af8db387eb19bd75699539966c667f654f5139a
monjara <yaginumee@gmail.com>
2022-05-06 21:54:29 +0900

    191-B

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,16 @@ use proconio::input;
 
 fn main() {
     input! {
-        s: usize,
-        t: usize,
+        n: usize,
+        x: usize,
     }
-    let mut ans = 0;
-    for a in 0..=s {
-        for b in 0..=s {
-            for c in 0..=s {
-                if a + b + c <= s && a * b * c <= t {
-                    ans += 1;
-                }
-            }
+    for _i in 0..n {
+        input! {
+            a: usize,
+        }
+        if a != x {
+            print!("{} ", a);
         }
     }
-    println!("{}", ans);
+    println!();
 }
